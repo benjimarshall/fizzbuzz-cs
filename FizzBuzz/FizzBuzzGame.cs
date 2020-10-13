9e3aacc6eaf5f9d9dcd03fb5edfa5eb3acff04c7
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-13 17:27:21 +0100

    Converted into IEnumerable Fizzbuzzer

---
+++ FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class FizzBuzzGame : IEnumerable<string>
+    {
+        private readonly int max;
+        private readonly Dictionary<int, bool> rules;
+
+        public FizzBuzzGame(int max, Dictionary<int, bool> rules)
+        {
+            this.max = max;
+            this.rules = rules;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new FizzBuzzer(max, rules);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
