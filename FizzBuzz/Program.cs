05e32c9363b89d77f1c6b974cff857431b54b304
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-13 13:13:14 +0100

    Moved FizzBuzz logic into separate method

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -9,51 +9,56 @@ namespace FizzBuzz
         {
             for (int i = 1; i <= 100; i++)
             {
-                List<string> results = new List<string>();
-                if (i % 3 == 0)
-                {
-                    results.Add("Fizz");
-                }
-
-                if (i % 5 == 0)
-                {
-                    results.Add("Buzz");
-                }
-
-                if (i % 7 == 0)
-                {
-                    results.Add("Bang");
-                }
-
-                if (i % 11 == 0)
-                {
-                    results.Clear();
-                    results.Add("Bong");
-                }
-
-                if (i % 13 == 0)
-                {
-                    var index = results.FindIndex(s => s.StartsWith("B"));
-
-                    // If none of the strings start with a "B" then index == -1
-                    // So change index to be the end of the list to append "Fezz" to
-                    index = index == -1 ? results.Count : index;
-
-                    results.Insert(index, "Fezz");
-                }
-
-                if (i % 17 == 0)
-                {
-                    results.Reverse();
-                }
-
-                if (results.Count == 0)
-                {
-                    results.Add(i.ToString());
-                }
-
-                Console.WriteLine(String.Join("", results));
+                Console.WriteLine(fizzbuzz(i));
             }
+        }
+
+        static string fizzbuzz(int i)
+        {
+            List<string> results = new List<string>();
+            if (i % 3 == 0)
+            {
+                results.Add("Fizz");
+            }
+
+            if (i % 5 == 0)
+            {
+                results.Add("Buzz");
+            }
+
+            if (i % 7 == 0)
+            {
+                results.Add("Bang");
+            }
+
+            if (i % 11 == 0)
+            {
+                results.Clear();
+                results.Add("Bong");
+            }
+
+            if (i % 13 == 0)
+            {
+                var firstBIndex = results.FindIndex(s => s.StartsWith("B"));
+
+                // If none of the strings start with a "B" then index == -1
+                // So change index to be the end of the list to append "Fezz" to
+                firstBIndex = firstBIndex == -1 ? results.Count : firstBIndex;
+
+                results.Insert(firstBIndex, "Fezz");
+            }
+
+            if (i % 17 == 0)
+            {
+                results.Reverse();
+            }
+
+            if (results.Count == 0)
+            {
+                results.Add(i.ToString());
+            }
+
+            return String.Join("", results);
         }
     }
 }
