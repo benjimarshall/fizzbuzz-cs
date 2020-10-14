6726fc78bfb6713f8aa02553075d1f35cc5eab73
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-14 13:43:45 +0100

    Refactoring

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -12,16 +12,18 @@ namespace FizzBuzz
 
             var max = InputUtils.GetMax(argsList);
 
-            Console.WriteLine(string.Join("\n",
-        Enumerable.Range(1, max)
-                    .Zip(Enumerable.Repeat("", max)) // Produce list of form [(1, ""), (2, ""), ...
-                    .Select<(int n, string msg), (int n, string msg)>(x => (x.n, x.msg + (x.n % 3 == 0 ? "Fizz" : "")))
-                    .Select<(int n, string msg), (int n, string msg)>(x => (x.n, x.msg + (x.n % 5 == 0 ? "Buzz" : "")))
-                    .Select<(int n, string msg), (int n, string msg)>(x => (x.n, x.msg + (x.n % 7 == 0 ? "Bang" : "")))
-                    // Add numbers in if they are not Fizz/Buzz/Bang
-                    .Select<(int n, string msg), (int n, string msg)>(x => (x.n, x.msg == "" ? x.n.ToString() : x.msg))
-                    .Select(x => x.msg.ToString())
-            ));
+            Console.WriteLine(
+              string.Join("\n",
+                Enumerable.Range(1, max)
+                  .Zip(Enumerable.Repeat("", max)) // Produce list of form [(1, ""), (2, ""), ...
+                  .Select<(int n, string message), (int, string)>(tuple => (tuple.n, tuple.message + (tuple.n % 3 == 0 ? "Fizz" : "")))
+                  .Select<(int n, string message), (int, string)>(tuple => (tuple.n, tuple.message + (tuple.n % 5 == 0 ? "Buzz" : "")))
+                  .Select<(int n, string message), (int, string)>(tuple => (tuple.n, tuple.message + (tuple.n % 7 == 0 ? "Bang" : "")))
+                  // Add numbers in if they are not Fizz/Buzz/Bang
+                  .Select<(int n, string message), (int, string)>(tuple => (tuple.n, tuple.message == "" ? tuple.n.ToString() : x.msg))
+                  .Select<(int n, string message), string>(tuple => tuple.message.ToString())
+              )
+            );
         }
     }
 }
